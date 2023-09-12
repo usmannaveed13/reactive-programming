1128885b97c072c907ec43916b3429615e7cbff4
Usman Naveed <usman.naveed@2iqresearch.com>
2023-09-12 16:11:06 +0500

    added test for getting async response

--- webflux1/src/test/java/com/reactive/webflux1/Lec01GetSingleResponseTest.java
+++ webflux1/src/test/java/com/reactive/webflux1/Lec01GetSingleResponseTest.java
@@ -4,6 +4,8 @@ import com.reactive.webflux1.dto.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 public class Lec01GetSingleResponseTest extends BaseTest{
 
@@ -21,5 +23,19 @@ public class Lec01GetSingleResponseTest extends BaseTest{
                 .block();
 
         System.out.println(response);
+    }
+
+    @Test
+    public void stepVerifierTest() {
+
+        Mono<Response> response = this.webClient
+                .get()
+                .uri("reactive-math/square/{number}", 5)
+                .retrieve()
+                .bodyToMono(Response.class);
+
+        StepVerifier.create(response)
+                .expectNextMatches(r-> r.getOutput() == 25)
+                .verifyComplete();
     }
 }
