cb4bd4b7ad99bab41ef509019f01180a719c521f
Usman Naveed <usman.naveed@2iqresearch.com>
2023-09-13 11:17:07 +0500

    added test for getting response for post request by using webClient

--- webflux1/src/main/java/com/reactive/webflux1/dto/MultiplyRequestDto.java
+++ webflux1/src/main/java/com/reactive/webflux1/dto/MultiplyRequestDto.java
@@ -1,10 +1,12 @@
 package com.reactive.webflux1.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
 @ToString
+@NoArgsConstructor
 public class MultiplyRequestDto {
     public MultiplyRequestDto(int first, int second) {
         this.first = first;
